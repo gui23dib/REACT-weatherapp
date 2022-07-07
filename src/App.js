56e03cbfd240556a63ac4dbba758ea570fc77b21
Guilherme Dib <gui23dib@gmail.com>
2022-07-07 11:02:04 -0300

    API FETCH AND CONSOLE TESTS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  const fetchData = () => {
+    return fetch('https://api.openweathermap.org/data/2.5/weather?lat=-23.944841&lon=-46.330376&appid=96b64657ba611c0c2fd06cfb8cb1a8ec')
+    .then((response) => response.json())
+    .then((actualData) => setData(actualData))
+    .then(console.log(data))
+  }
+
+  /*useEffect(() => {
+    fetchData();
+  });*/
+
+  function handleFetch(){
+    fetchData();
+  }
+
+  function handleMap(){
+    console.log("Latitude " + data.coord.lat)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={handleFetch}>Fetch</button>
+      <button onClick={handleMap}>Map</button>
+      <h1>Hello World!</h1>
     </div>
   );
 }
