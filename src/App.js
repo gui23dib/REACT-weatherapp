91f0773d552594f6b94a79c6b746672a8fd71294
Guilherme Dib <gui23dib@gmail.com>
2022-07-08 16:01:53 -0300

    NON MODULARIZED CONVERSIONS AND CONSISTENT LAYOUT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import './styles/weatherApp.css'
 
 function App() {
 
@@ -16,9 +17,7 @@ function App() {
     fetchData();
   }, []);
 
-
   function checkBackgorund(type){ //OBJECT LITERAL
-
     const currentMainWeather = { //FOLLOWING MAIN AND ID PROVIDED IN data.weather[0]
       "Thunderstorm" : "thunderApp",
       "Drizzle" : "drizzleApp",
@@ -35,37 +34,32 @@ function App() {
       "Smoke" : "fogApp",
       default: "defaultApp"
     }
-
     return currentMainWeather[type] || currentMainWeather.default
-
   }
 
-  /*function handleFetch(){
-    fetchData();
-  }*/ //MANUAL ON CLICK FETCH <button onClick={handleFetch}>Fetch</button>
+  let celsiusConverter = (Ktemp) => { return (Ktemp - 273.15).toFixed(2) + "° C" }
+  let farenheitConverter = (Ktemp) => { return ((Ktemp - 273.15)* (9/5) + 32).toFixed(2) + "° F" }
+
+  let convertToCelsius = false //AD HANDLE CONVERSION TO A BUTTOn
 
   return (
     <div className='appContainer'>
       <div className={checkBackgorund(data?.weather[0]?.main)} id='genericApp'>
           <div className='localContainer'>
-            <h3>Name: {data?.name}</h3>
-            <h3>Latitude: {data?.coord?.lat}</h3>
-            <h3>Longitude: {data?.coord?.lon}</h3>
+            <h3><b>{data?.name}</b></h3>
+            <h3><sup>{data?.weather[0]?.description.charAt(0).toUpperCase() + data?.weather[0]?.description.slice(1)}</sup></h3>
+            <h3><span>latitude:</span>{data?.coord?.lat}</h3>
+            <h3><span>longitude:</span>{data?.coord?.lon}</h3>
           </div>
           <div className='mainContainer'>
-            <h3>feels_like: {data?.main?.feels_like}</h3>
-            <h3>humidity: {data?.main?.humidity}</h3>
+            <h3><span>sensação térmica</span>{celsiusConverter(data?.main?.feels_like)}</h3>
+            <h3><span>humidade</span>{data?.main?.humidity + "%"}</h3>
+            <h3><span>ventos</span>{data?.wind?.speed*3.6 + " km/h"}</h3>
           </div>
           <div className='tempContainer'>
-            <h3>temperature maximum:{data?.main?.temp_max}</h3>
-            <h3>temperature: (temp) {data?.main?.temp}</h3>
-            <h3>temperature minimum:{data?.main?.temp_min}</h3>
-          </div>
-          <div className='weatherContainer'>
-            <h3>{data?.weather[0]?.description}</h3>
-            <h3>{data?.weather[0]?.icon}</h3>
-            <h3>{data?.weather[0]?.id}</h3>
-            <h3>{data?.weather[0]?.main}</h3>
+            <h3><span>temperatura maxima</span>{convertToCelsius ? celsiusConverter(data?.main?.temp_max) : farenheitConverter(data?.main?.temp_max)}<u>  ({!convertToCelsius ? celsiusConverter(data?.main?.temp_max) : farenheitConverter(data?.main?.temp_max)})</u></h3>
+            <h3><span>temperatura atual</span>{convertToCelsius ? celsiusConverter(data?.main?.temp) : farenheitConverter(data?.main?.temp)}<u>  ({!convertToCelsius ? celsiusConverter(data?.main?.temp_max) : farenheitConverter(data?.main?.temp_max)})</u></h3>
+            <h3><span>temperatura minima</span>{convertToCelsius ? celsiusConverter(data?.main?.temp_min) : farenheitConverter(data?.main?.temp_min)}<u>  ({!convertToCelsius ? celsiusConverter(data?.main?.temp_max) : farenheitConverter(data?.main?.temp_max)})</u></h3>
           </div>
       </div>
     </div>
